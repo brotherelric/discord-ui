ba40db209c668a064953329f9a809d74aa1da4d6
kuso-senpai <bellou9022@gmail.com>
2021-10-12 15:34:32 +0200

    fixed BadArgument import error

--- discord_ui/errors.py
+++ discord_ui/errors.py
@@ -1,6 +1,9 @@
-from discord.ext import commands
+try:
+    from discord.ext.commands.errors import BadArgument
+except ImportError:
+    from discord.ext.commands import BadArgument
 
-class InvalidLength(commands.BadArgument):
+class InvalidLength(BadArgument):
     """This exception is thrown whenever a invalid length was provided"""
     def __init__(self, my_name, _min=None, _max=None, *args: object) -> None:
         if _min is not None and _max is not None:
@@ -10,23 +13,23 @@ class InvalidLength(commands.BadArgument):
         elif _min is not None and _max is None:
             err = "Lenght of '" + my_name + "' must be more than " + str(_min)
         super().__init__(err)
-class OutOfValidRange(commands.BadArgument):
+class OutOfValidRange(BadArgument):
     """This exception is thrown whenever a value was ot of its valid range"""
     def __init__(self, name, _min, _max, *args: object) -> None:
         super().__init__("'" + name + "' must be in range " + str(_min) + " and " + str(_max))
-class WrongType(commands.BadArgument):
+class WrongType(BadArgument):
     """This exception is thrown whenever a value is of the wrong type"""
     def __init__(self, name, me, valid_type, *args: object) -> None:
         super().__init__("'" + name + "' must be of type " + (str(valid_type) if not isinstance(valid_type, list) else ' or '.join(valid_type)) + ", not " + str(type(me)))
-class InvalidEvent(commands.BadArgument):
+class InvalidEvent(BadArgument):
     """This exception is thrown whenever a invalid eventname was passed"""
     def __init__(self, name, events, *args: object) -> None:
         super().__init__("Invalid event name, event must be " + " or ".join(events) + ", not " + str(name))
-class MissingListenedComponentParameters(commands.BadArgument):
+class MissingListenedComponentParameters(BadArgument):
     """This exception is thrown whenever a callback for a listening component is missing parameters"""
     def __init__(self, *args: object) -> None:
         super().__init__("Callback function for listening components needs to accept one parameter (the used component)", *args)
-class CouldNotParse(commands.BadArgument):
+class CouldNotParse(BadArgument):
     """This exception is thrown whenever the libary was unable to parse the data with the given method"""
     def __init__(self, data, type, method, *args: object) -> None:
         super().__init__("Could not parse '" + str(data) + " [" + str(type) + "]' with method " + str(method), *args)
